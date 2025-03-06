be2b1b3a06d8a5098bf15fd4f423c49c954701c1
CSE  Ground Station <nathan.tipton@arcfield.com>
2025-03-06 15:45:36 -0700

    made it so that it automatically cleaned up after itself and removes the logs_test folder

--- testing/tests/logger_test.py
+++ testing/tests/logger_test.py
@@ -11,7 +11,7 @@ from logging_system_display_python_api.logger import loggerCustom
 logger_folder = "logging_system_display_python_api/testing/logs_test/"
 
 @pytest.mark.logger_tests
-def test_logger_init_logs_folder_not_exist():
+def test_logger_init_logs_folder_not_exist(request):
     try:
         shutil.rmtree(logger_folder)
     except FileNotFoundError:
@@ -21,8 +21,10 @@ def test_logger_init_logs_folder_not_exist():
 
     assert os.path.isdir(logger_folder)
 
+    request.addfinalizer(clean_up)
+
 @pytest.mark.logger_tests
-def test_logger_init_logs_folder_does_exist():
+def test_logger_init_logs_folder_does_exist(request):
     try:
         os.mkdir(logger_folder)
     except FileExistsError:
@@ -32,19 +34,32 @@ def test_logger_init_logs_folder_does_exist():
 
     assert os.path.isdir(logger_folder)
 
+    request.addfinalizer(clean_up)
+
 @pytest.mark.logger_tests
-def test_logger_send_log():
+def test_logger_send_log(request):
     test_logger = loggerCustom(f"{logger_folder}test.txt", log_directory=logger_folder)
     test_logger.send_log("this is a log test")
 
     with open(f"{logger_folder}test.txt", 'r') as file:
         assert re.match(r".*: this is a log test\n", file.read())
+    
+    request.addfinalizer(clean_up)
 
 @pytest.mark.logger_tests
-def test_logger_send_two_logs():
+def test_logger_send_two_logs(request):
     test_logger = loggerCustom(f"{logger_folder}test.txt", log_directory=logger_folder)
     test_logger.send_log("this is a log test")
     test_logger.send_log("this is another log test")
 
     with open(f"{logger_folder}test.txt", 'r') as file:
         assert re.match(r".*: this is a log test\n.*: this is another log test\n", file.read())
+    
+    request.addfinalizer(clean_up)
+
+def clean_up():
+    try:
+        shutil.rmtree(logger_folder)
+    except FileNotFoundError:
+        print("flag")
+        pass
